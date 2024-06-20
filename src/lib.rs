27c88f51a25b3e91d14fdc309d82a21d4004f808
Mark Lodato <marklodato0@gmail.com>
2024-06-19 20:08:52 -0400

    Fix stream wakeup and add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
-// 1. Rename Outersperse to Outerleave or Unzip or something lamer
 // 2. Documentation
 // 3. Code Organization
-//    - Consolidate structs with const generics?
-// 4. Better tests including Loom and Miri
+//    - Consolidate structs with const generics? (pin_project doesn't handle this)
+// 4. Better tests including Loom, Miri (can this work with tokio?), and quickcheck or proptest
 // 5. Performance optimizations
-//    - Improved Waking functionality (Even should Wake Odd)
 //    - Get rid of Mutex (AtomicBool governs mutual exclusion)
 //    - Improved Ordering variant
 
 use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll};
+use std::task::{Context, Poll, Waker};
 
 use futures::{ready, Stream};
 use pin_project_lite::pin_project;
@@ -21,20 +19,22 @@ pub trait Outerleave {
     where
         Self: Sized;
 }
+struct SharedState<S> {
+    stream: Mutex<S>,
+    odd_next: AtomicBool,
+    odd_waker: Mutex<Option<Waker>>,
+    even_waker: Mutex<Option<Waker>>,
+}
 pin_project! {
     pub struct Even<S> {
         #[pin]
-        stream: Arc<Mutex<S>>,
-        #[pin]
-        odd_next: Arc<AtomicBool>,
+        shared_state: Arc<SharedState<S>>,
     }
 }
 pin_project! {
     pub struct Odd<S> {
         #[pin]
-        stream: Arc<Mutex<S>>,
-        #[pin]
-        odd_next: Arc<AtomicBool>,
+        shared_state: Arc<SharedState<S>>,
     }
 }
 
@@ -44,12 +44,14 @@ impl<S: Stream> Stream for Even<S> {
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
 
-        let odd_next = this.odd_next.load(Ordering::SeqCst);
+        let odd_next = this.shared_state.odd_next.load(Ordering::SeqCst);
         if odd_next {
+            let mut waker = this.shared_state.even_waker.lock().unwrap();
+            *waker = Some(cx.waker().clone());
             return Poll::Pending;
         }
 
-        let inner_stream = &mut *this.stream.lock().expect("Ignoring poisoning for now");
+        let inner_stream = &mut *this.shared_state.stream.lock().unwrap();
 
         // SAFETY: This probably isn't actually safe, but I'm hoping the fact that we own this
         // stream and we maybe promise not to move it is fine. We'll test with MIRI later and still
@@ -57,7 +59,10 @@ impl<S: Stream> Stream for Even<S> {
         let mut inner_stream = unsafe { Pin::new_unchecked(inner_stream) };
         let next_item = ready!(inner_stream.as_mut().poll_next(cx));
 
-        this.odd_next.store(true, Ordering::SeqCst);
+        this.shared_state.odd_next.store(true, Ordering::SeqCst);
+        if let Some(waker) = this.shared_state.odd_waker.lock().unwrap().take() {
+            waker.wake();
+        }
         Poll::Ready(next_item)
     }
 }
@@ -67,12 +72,14 @@ impl<S: Stream> Stream for Odd<S> {
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
 
-        let odd_next = this.odd_next.load(Ordering::SeqCst);
+        let odd_next = this.shared_state.odd_next.load(Ordering::SeqCst);
         if !odd_next {
+            let mut waker = this.shared_state.odd_waker.lock().unwrap();
+            *waker = Some(cx.waker().clone());
             return Poll::Pending;
         }
 
-        let inner_stream = &mut *this.stream.lock().expect("Ignoring poisoning for now");
+        let inner_stream = &mut *this.shared_state.stream.lock().unwrap();
 
         // SAFETY: This probably isn't actually safe, but I'm hoping the fact that we own this
         // stream and we maybe promise not to move it is fine. We'll test with MIRI later and still
@@ -80,21 +87,27 @@ impl<S: Stream> Stream for Odd<S> {
         let mut inner_stream = unsafe { Pin::new_unchecked(inner_stream) };
         let next_item = ready!(inner_stream.as_mut().poll_next(cx));
 
-        this.odd_next.store(false, Ordering::SeqCst);
+        this.shared_state.odd_next.store(false, Ordering::SeqCst);
+        if let Some(waker) = this.shared_state.even_waker.lock().unwrap().take() {
+            waker.wake();
+        }
         Poll::Ready(next_item)
     }
 }
 
 impl<S: Stream> Outerleave for S {
     fn outerleave(self) -> (Even<S>, Odd<S>) {
-        let stream = Arc::new(Mutex::new(self));
-        let odd_next = Arc::new(AtomicBool::new(false));
+        let shared_state = Arc::new(SharedState {
+            stream: Mutex::new(self),
+            odd_next: AtomicBool::new(false),
+            odd_waker: Mutex::new(None),
+            even_waker: Mutex::new(None),
+        });
         (
             Even {
-                stream: Arc::clone(&stream),
-                odd_next: Arc::clone(&odd_next),
+                shared_state: Arc::clone(&shared_state),
             },
-            Odd { stream, odd_next },
+            Odd { shared_state },
         )
     }
 }
@@ -103,7 +116,9 @@ impl<S: Stream> Outerleave for S {
 mod tests {
     use super::*;
 
+    use async_stream::stream;
     use futures::{FutureExt, StreamExt};
+    use itertools::Itertools;
 
     #[tokio::test]
     async fn outerleaves() {
@@ -121,5 +136,32 @@ mod tests {
         });
         assert_eq!(odds.next().await, Some(3));
         jh.await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn handles_different_yielding_patterns() {
+        let sleeps: Vec<_> = (0..6)
+            .map(|n| std::time::Duration::from_millis(n * 10))
+            .collect();
+        let sleeps = sleeps.iter().copied().permutations(sleeps.len()).take(300);
+        for sleeps in sleeps {
+            let stream = stream! {
+                for (i, sleep) in (0..6).zip(sleeps) {
+                    tokio::time::sleep(sleep).await;
+                    yield Ok(i);
+                }
+            };
+
+            let (evens, odds) = stream.outerleave();
+            let (tx, rx) = futures::channel::mpsc::unbounded();
+            let even_fut = evens.forward(tx.clone());
+            let odd_fut = odds.forward(tx);
+            let (even_result, odd_result) = futures::join!(even_fut, odd_fut);
+            even_result.unwrap();
+            odd_result.unwrap();
+
+            let received: Vec<_> = rx.collect().await;
+            assert_eq!(received, [0, 1, 2, 3, 4, 5]);
+        }
     }
 }
