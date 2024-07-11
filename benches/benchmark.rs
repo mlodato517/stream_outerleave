639f50525921257f8a82281b3a9080a2d9966637
Mark Lodato <marklodato0@gmail.com>
2024-07-10 21:36:26 -0400

    Improve benchmarks
    ## This Commit
    
    Improves the benchmarks in a few ways:
    
    1. Tests for `poll_next` use a `NoopWaker` and synchronous testing to
       ensure as little overhead as possible.
    2. `poll_pending` test reuses the same `Waker` to hopefully catch the
       `clone_from` optimization.
    3. `collect` and `spawning` benchmarks no longer waste time collecting
       results.
    4. `spawning` benchmark updated to not use `outerleave` since the point
       was to compare outerleaving to not outerleaving, not outerleaving to
       spawned outerleaving.

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,37 +1,50 @@
+use std::sync::Arc;
+use std::task::{Context, Wake, Waker};
+
 use criterion::BenchmarkId;
 use criterion::Criterion;
 use criterion::{criterion_group, criterion_main};
-use futures::StreamExt;
+use futures::{Stream, StreamExt};
 
 use stream_split::Outerleave;
 
+#[derive(Clone, Copy)]
+struct NoopWaker;
+impl Wake for NoopWaker {
+    fn wake(self: Arc<Self>) {}
+}
+
 fn outerleave(c: &mut Criterion) {
+    let waker = Arc::new(NoopWaker);
+    let waker = Waker::from(waker);
+    let mut context = Context::from_waker(&waker);
+
     c.bench_with_input(BenchmarkId::new("poll_ready", ""), &(), |b, _| {
-        b.to_async(tokio::runtime::Runtime::new().unwrap())
-            .iter_batched(
-                || futures::stream::iter([0, 1, 2, 3, 4]).outerleave().0,
-                |mut even| async move { futures::poll!(even.next()) },
-                criterion::BatchSize::SmallInput,
-            );
+        b.iter_batched(
+            || futures::stream::iter([0, 1, 2, 3, 4]).outerleave().0,
+            |mut even| {
+                let mut pinned = std::pin::pin!(even);
+                pinned.as_mut().poll_next(&mut context)
+            },
+            criterion::BatchSize::SmallInput,
+        );
     });
     c.bench_with_input(BenchmarkId::new("poll_pending", ""), &(), |b, _| {
-        b.to_async(tokio::runtime::Runtime::new().unwrap())
-            .iter_batched(
-                || futures::stream::iter([0, 1, 2, 3, 4]).outerleave().1,
-                |mut odd| async move { futures::poll!(odd.next()) },
-                criterion::BatchSize::SmallInput,
-            );
+        let (_, mut odd) = futures::stream::iter([0, 1, 2, 3, 4]).outerleave();
+        let mut pinned = std::pin::pin!(odd);
+        // Poll once to store waker
+        let _ = pinned.as_mut().poll_next(&mut context);
+        b.iter(|| pinned.as_mut().poll_next(&mut context));
     });
-    c.bench_with_input(BenchmarkId::new("collect", ""), &(), |b, _| {
+    c.bench_with_input(BenchmarkId::new("outerleave", ""), &(), |b, _| {
         b.to_async(tokio::runtime::Runtime::new().unwrap())
             .iter_batched(
                 || futures::stream::iter([Ok(0), Ok(1), Ok(2), Ok(3), Ok(4)]).outerleave(),
                 |(even, odd)| async move {
-                    let (tx, rx) = futures::channel::mpsc::unbounded();
+                    let (tx, _rx) = futures::channel::mpsc::unbounded();
                     let even_fut = even.forward(tx.clone());
                     let odd_fut = odd.forward(tx);
-                    let _ = futures::join!(even_fut, odd_fut);
-                    rx.collect::<Vec<_>>().await
+                    futures::join!(even_fut, odd_fut)
                 },
                 criterion::BatchSize::SmallInput,
             );
@@ -39,23 +52,23 @@ fn outerleave(c: &mut Criterion) {
     c.bench_with_input(BenchmarkId::new("spawning", ""), &(), |b, _| {
         b.to_async(tokio::runtime::Runtime::new().unwrap())
             .iter_batched(
-                || futures::stream::iter([Ok::<_, ()>(0), Ok(1), Ok(2), Ok(3), Ok(4)]).outerleave(),
-                |(mut even, mut odd)| async move {
-                    let (tx, rx) = futures::channel::mpsc::unbounded();
+                || futures::stream::iter([Ok::<_, ()>(0), Ok(1), Ok(2), Ok(3), Ok(4)]),
+                |mut stream| async move {
+                    let (even_tx, _rx) = futures::channel::mpsc::unbounded();
+                    let (odd_tx, _rx) = futures::channel::mpsc::unbounded();
                     let jh = tokio::spawn(async move {
                         loop {
-                            if let Some(next) = even.next().await {
-                                tx.unbounded_send(next).unwrap();
+                            if let Some(next) = stream.next().await {
+                                even_tx.unbounded_send(next).unwrap();
                             }
-                            if let Some(next) = odd.next().await {
-                                tx.unbounded_send(next).unwrap();
+                            if let Some(next) = stream.next().await {
+                                odd_tx.unbounded_send(next).unwrap();
                             } else {
                                 break;
                             }
                         }
                     });
-                    jh.await.unwrap();
-                    rx.collect::<Vec<_>>().await
+                    jh.await
                 },
                 criterion::BatchSize::SmallInput,
             )
